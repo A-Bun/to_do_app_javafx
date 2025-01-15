3f3e8a55fba596664e79bbb4cb4e5eb39c19bfb9
Alexis Peoples <techpeoples@kutztown.edu>
2025-01-15 14:58:57 -0500

    Updating styles and usage (TBC)

--- src/main/java/com/todo/PrimaryController.java
+++ src/main/java/com/todo/PrimaryController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -19,6 +20,7 @@ public class PrimaryController implements Initializable{
     @FXML private Button all_lists_edit;
     @FXML private Button global_settings;
     private Boolean editing = false;
+    final private int container_cnt = 2;
     private int cntr;
 
     @FXML
@@ -46,14 +48,20 @@ public class PrimaryController implements Initializable{
             all_lists_edit.setDisable(true);
             new_list_trigger.setDisable(true);
             HBox new_list_box = new HBox();
+            new_list_box.setFillHeight(true);
+            new_list_box.setMaxWidth(list_container.getWidth());
+            new_list_box.setAlignment(Pos.CENTER_LEFT);
+            new_list_box.setSpacing(5);
             Button new_list = new Button();
+            Button remove_new_list = new Button();
             new_list.getStyleClass().add("container_child");
-            new_list.setMaxWidth(list_container.getWidth());
+            remove_new_list.getStyleClass().add("delete_button");
             new_list.setOnAction((ActionEvent ae) -> {
                 try {
                     switchToSecondary();
                 } catch (IOException e1) {
             }});
+            remove_new_list.setText("X");
 
             TextField list_name = new TextField();
             list_name.setPromptText("Enter new list title here...");
@@ -65,10 +73,21 @@ public class PrimaryController implements Initializable{
                     new_list.setText(list_name.getText().trim());
                     createNewList(new_list_box, new_list);
                     new_list_box.getChildren().remove(list_name);
+                    new_list_box.getChildren().remove(remove_new_list);
                 }
             });
 
-            new_list_box.getChildren().add(list_name);
+            remove_new_list.setOnAction((ActionEvent ae) -> {
+                list_container.getChildren().remove(new_list_box);
+
+                if(list_container.getChildren().size() > container_cnt) {
+                    all_lists_edit.setDisable(false);
+                }
+
+                new_list_trigger.setDisable(false);
+            });
+
+            new_list_box.getChildren().addAll(remove_new_list, list_name);
 
             list_container.getChildren().add(list_container.getChildren().size()-1, new_list_box);
         });
@@ -105,6 +124,8 @@ public class PrimaryController implements Initializable{
         System.out.println("Edit Mode On");
 
         list_container.getChildren().get(list_container.getChildren().size()-1).setDisable(true);
+        all_lists_edit.getStyleClass().remove(all_lists_edit.getStyleClass().size()-1);
+        all_lists_edit.getStyleClass().add("edit_button_on");
 
         for (cntr = 1; cntr < list_container.getChildren().size()-1; cntr++) {
             HBox child_list_hbox = (HBox)list_container.getChildren().get(cntr);
@@ -115,10 +136,15 @@ public class PrimaryController implements Initializable{
             rename_list.getStyleClass().add("container_child");
             child_list_hbox.getChildren().add(rename_list);
 
-            list_button.setText("DELETE");
+            list_button.setText("X");
+            list_button.getStyleClass().remove(list_button.getStyleClass().size()-1);
+            list_button.getStyleClass().add("delete_button");
             list_button.setOnAction((ActionEvent e) -> {
-                cntr--;
                 list_container.getChildren().remove(child_list_hbox);
+                if(list_container.getChildren().size() <= container_cnt) {
+                    editing = !editing;
+                    concludeEditMode();
+                }
             });
         }
     }
@@ -136,6 +162,8 @@ public class PrimaryController implements Initializable{
         }
         
         System.out.println("Edit Mode Off");
+        all_lists_edit.getStyleClass().remove(all_lists_edit.getStyleClass().size()-1);
+        all_lists_edit.getStyleClass().add("edit_button");
 
         for (cntr = 1; cntr < list_container.getChildren().size()-1; cntr++) {
             HBox child_list_hbox = (HBox)list_container.getChildren().get(cntr);
@@ -144,6 +172,8 @@ public class PrimaryController implements Initializable{
             TextField renamed_list = (TextField)child_list_hbox.getChildren().get(child_list_hbox.getChildren().size()-1);
 
             list_button.setText(renamed_list.getText());
+            list_button.getStyleClass().remove(list_button.getStyleClass().size()-1);
+            list_button.getStyleClass().add("container_child");
             list_button.setOnAction((ActionEvent e) -> {
                 try {
                     switchToSecondary();
@@ -155,7 +185,7 @@ public class PrimaryController implements Initializable{
 
         list_container.getChildren().get(list_container.getChildren().size()-1).setDisable(false);
 
-        if(list_container.getChildren().size() <= 2) {
+        if(list_container.getChildren().size() <= container_cnt) {
             all_lists_edit.setDisable(true);
         }
     }
