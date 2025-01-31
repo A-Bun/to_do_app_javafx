812f538759047beba49160662733396e8ab7afe0
Alexis Peoples <techpeoples@kutztown.edu>
2025-01-31 15:11:48 -0500

    Edited styles to fit better for specific lists

--- src/main/java/com/todo/SpecificListController.java
+++ src/main/java/com/todo/SpecificListController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
@@ -25,6 +26,7 @@ public class SpecificListController implements Initializable {
     @FXML private BorderPane border_pane;
     @FXML private ScrollPane center_content;
     @FXML private Label specific_list_label;
+    @FXML private Button all_lists_button;
     @FXML private Button specific_list_edit;
     @FXML private HBox top_content;
     @FXML private VBox list_container;
@@ -38,6 +40,9 @@ public class SpecificListController implements Initializable {
         border_pane.setLeft(null);
         border_pane.setBottom(null);
         border_pane.setCenter(center_content);
+
+        all_lists_button.getStyleClass().add("all_lists_border");
+        specific_list_edit.getStyleClass().add("edit_border");
         
         String current_list = App.getSpecificList();
         specific_list_label.setText(current_list);
@@ -118,26 +123,23 @@ public class SpecificListController implements Initializable {
         /* loop over current list items and add new ones as TextFields second to last in list_container (before Add Item button) */
         // specific_list_edit.getStyleClass().remove(specific_list_edit.getStyleClass().size()-1);
         specific_list_edit.getStyleClass().add("edit_button_on");
-
-        ContextMenu label_menu = new ContextMenu();
-        MenuItem label_rename = new MenuItem("Rename");
-        label_rename.setOnAction((ActionEvent e) -> {
-            TextField rename_item = new TextField(specific_list_label.getText());
-            rename_item.getStyleClass().add("container_sub_child_tf");
-            rename_item.setMaxWidth(500);
-            HBox.setHgrow(rename_item, Priority.ALWAYS);
-            rename_item.setOnAction((ActionEvent ae) -> {
-            String trimmed_name = rename_item.getText().trim();
-            if(!trimmed_name.isBlank()) {
-                specific_list_label.setText(trimmed_name);
-                top_content.getChildren().remove(rename_item);
-                top_content.getChildren().add(1, specific_list_label);
-            }});
-            top_content.getChildren().remove(specific_list_label);
-            top_content.getChildren().add(1, rename_item);
-        });
-        label_menu.getItems().add(label_rename);
-        specific_list_label.setContextMenu(label_menu);
+        specific_list_label.setContextMenu(addNewMenuItem(new ContextMenu(), "Rename", (ActionEvent e) -> {
+                TextField rename_item = new TextField(specific_list_label.getText());
+                rename_item.getStyleClass().add("list_header_tf");
+                rename_item.setMaxWidth(500);
+                rename_item.setPrefHeight(all_lists_button.getHeight());
+                HBox.setHgrow(rename_item, Priority.ALWAYS);
+                rename_item.setOnAction((ActionEvent ae) -> {
+                String trimmed_name = rename_item.getText().trim();
+                if(!trimmed_name.isBlank()) {
+                    specific_list_label.setText(trimmed_name);
+                    top_content.getChildren().remove(rename_item);
+                    top_content.getChildren().add(1, specific_list_label);
+                }});
+                top_content.getChildren().remove(specific_list_label);
+                top_content.getChildren().add(1, rename_item);
+                HBox.setMargin(rename_item, specific_list_label.getInsets());
+            }));
 
         for (int i = 0; i < list_container.getChildren().size()-1; i++) {
             // get the button
@@ -145,23 +147,19 @@ public class SpecificListController implements Initializable {
 
             Button item = (Button)child_list_hbox.getChildren().get(0);
 
-            ContextMenu item_menu = new ContextMenu();
-            MenuItem menu_rename = new MenuItem("Rename");
-            menu_rename.setOnAction((ActionEvent e) -> {
-                TextField rename_item = new TextField(item.getText());
-                rename_item.getStyleClass().add("container_sub_child_tf");
-                rename_item.setOnAction((ActionEvent ae) -> {
-                    String trimmed_name = rename_item.getText().trim();
-                    if(!trimmed_name.isBlank()) {
-                        item.setText(trimmed_name);
-                        child_list_hbox.getChildren().remove(rename_item);
-                        child_list_hbox.getChildren().add(item);
-                    }});
-                child_list_hbox.getChildren().remove(item);
-                child_list_hbox.getChildren().add(rename_item);
-            });
-            item_menu.getItems().add(menu_rename);
-            item.setContextMenu(item_menu);
+            item.setContextMenu(addNewMenuItem(new ContextMenu(), "Rename", (ActionEvent e) -> {
+                    TextField rename_item = new TextField(item.getText());
+                    rename_item.getStyleClass().add("container_sub_child_tf");
+                    rename_item.setOnAction((ActionEvent ae) -> {
+                        String trimmed_name = rename_item.getText().trim();
+                        if(!trimmed_name.isBlank()) {
+                            item.setText(trimmed_name);
+                            child_list_hbox.getChildren().remove(rename_item);
+                            child_list_hbox.getChildren().add(item);
+                        }});
+                    child_list_hbox.getChildren().remove(item);
+                    child_list_hbox.getChildren().add(rename_item);
+                }));
         }
     }
 
@@ -170,10 +168,16 @@ public class SpecificListController implements Initializable {
 
         /* Remove "Add Item" button from the list_container */
         specific_list_edit.getStyleClass().remove(specific_list_edit.getStyleClass().size()-1);
-        specific_list_edit.getStyleClass().add("edit_button");
         list_container.getChildren().remove(list_container.getChildren().size()-1);
 
         /* loop over current list items, change them back to buttons */
+    }
+
+    private ContextMenu addNewMenuItem(ContextMenu menu, String menu_item_name, EventHandler<ActionEvent> menu_item_event) {
+        MenuItem menu_item = new MenuItem(menu_item_name);
+        menu_item.setOnAction(menu_item_event);
+        menu.getItems().add(menu_item);
+        return menu;
     }
 
     @FXML
